d4bb07c0815891a8a99a981ecc7d801b7d82dccd
Gerry Donnelly <donneger33@gmail.com>
2021-04-17 17:57:12 +0100

    updating all of the tables

--- code/analysis.py
+++ code/analysis.py
@@ -2,19 +2,19 @@
 # Progarmming and Scripting 2021
 # Author: Gerard Donnelly
 
-# Import the libraries needed for the program
+# Import the libraries needed for the program.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-#set up the variable to hold the dataset
-irisdata = "irisdata.csv"
+#set up the variable to hold the dataset that is stored as irisdata,csv.
+irisdata = (r'tables/irisdata.csv')
 
 #Read in the data set and add the column names per the attribute information below:
-irisdf = pd.read_csv(irisdata, names=["Sepal_Length", "Sepal_Width" , "Petal_Length", "Petal_Width", "Class"])
-	#Attribute Information:
+irisdf = pd.read_csv(irisdata, names=['Sepal_Length', 'Sepal_Width' , 'Petal_Length', 'Petal_Width', 'Class'])
+#Attribute Information in the dataset:
 	#1. sepal length in cm
 	#2. sepal width in cm
 	#3. petal length in cm
@@ -24,138 +24,131 @@ irisdf = pd.read_csv(irisdata, names=["Sepal_Length", "Sepal_Width" , "Petal_Len
 	#-- Iris Versicolour
 	#-- Iris Virginica
 
-#This function creates the plots for the statistical analysis - Summary Plots
-
-
-# Access to program functions from user menu function. 
-def main():
-	display_menu()
-	while True:
-		choice = input("Enter choice: ")
-		if (choice == "1"):
-			view_data()
-			display_menu()
-		elif (choice == "2"):
-			view_stats()
-			display_menu()
-		elif (choice == "3"):
-		
-			display_menu()
-		elif (choice == "4"):
-			print("Thank you, program now exits.")
-			break
-
-
-# Function to display user menu.
-def display_menu():
-    print("\nFishers Iris Dataset, a Python Analysis")
-    print("-"*39)
-    print("User MENU")
-    print("="*9)
-    print("1 - View Dataset")
-    print("2 - Run Statistics")
-    print("3 - Generate Plots")
-    print("4 - Exit Application")
-
-
-#This function imports the raw Iris dataset, generates a number of summaries and saves results to csv files.   
 #https://archive.ics.uci.edu/ml/datasets/iris
 #https://pandas.pydata.org/docs/user_guide/index.html
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html
-def view_data():
-    
-#Summarise the data set and transpose for readibility
-	datasummary = (irisdf.describe()).transpose()
-	datahead = (irisdf.head(5))
-	datatail = irisdf.tail(5)
 
+#Summarise the data set and transpose for readibility
+datasummary = (irisdf.describe()).transpose()
+datahead = (irisdf.head(5))
+datatail = irisdf.tail(5)
 
 # Output the Basic characteristics of the dataset to the screen. 	https://pandas.pydata.org/pandas-docs/stable/user_guide/basics.html
-	print("\n1. This shows the data type of the set.\n", "\t",type(irisdf))
-	print("\n2. This shows the shape of the data, i.e. number of rows and columns.\n","\t",irisdf.shape)
-	print("\n3. Overall summary of the data\n", datasummary, "\n")
-	print("\n4. First 5 lines of the dataset\n",irisdf.head(5), "\n")
-	print("\n5. Last 5 lines of the dataset\n",irisdf.tail(5),"\n")
-
+print('\n1. This shows the data type of the set.\n', '\t',type(irisdf))
+print('\n2. This shows the shape of the data, i.e. number of rows and columns.\n','\t',irisdf.shape)
+print('\n3. Overall summary of the data\n', datasummary, '\n')
+print('\n4. First 5 lines of the dataset\n',irisdf.head(5), '\n')
+print('\n5. Last 5 lines of the dataset\n',irisdf.tail(5),'\n')
 
 #Write the resulting summary tables to csv files. 
-	datasummary.to_csv("datasummary.csv")
-	datahead.to_csv("datahead.csv")
-	datatail.to_csv("datatail.csv")	
-	
-	return
+datasummary.to_csv(r'tables/datasummary.csv')
+datahead.to_csv(r'tables/datahead.csv')
+datatail.to_csv(r'tables/datatail.csv')	
 
-
-#This function performs a preliminary statistical analysis - Summary Statistics
-def view_stats():
-    	
-#Set up the basic statistics of the dataset. 	
-	classcount = irisdf.groupby("Class").count()
-	classmean = irisdf.groupby("Class").mean()
-	classmax = irisdf.groupby("Class").max()
-	classmin = irisdf.groupby("Class").min()
-	classmedian = irisdf.groupby("Class").median()
-	classstd = irisdf.groupby("Class").std()
+# Set up the variables to hold the basic statistical characteristicsa of the dataset, grouped by Iris species. 
+classcount = irisdf.groupby('Class').count()
+classmean = irisdf.groupby('Class').mean()
+classmax = irisdf.groupby('Class').max()
+classmin = irisdf.groupby('Class').min()
+classmedian = irisdf.groupby('Class').median()
+classstd = irisdf.groupby('Class').std()
+classcor= irisdf.corr()
 
 # Output the resulting details to csv files. 
-	classcount.to_csv("classcount.csv")
-	classmean.to_csv("classmean.csv")
-	classmax.to_csv("classmax.csv")
-	classmin.to_csv("classmin.csv")
-	classmedian.to_csv("classmedian.csv")
-	classstd.to_csv("classstd.csv")
-	
-	
+classcount.to_csv(r'tables/classcount.csv')
+classmean.to_csv(r'tables/classmean.csv')
+classmax.to_csv(r'tables/classmax.csv')
+classmin.to_csv(r'tables/classmin.csv')
+classmedian.to_csv(r'tables/classmedian.csv')
+classstd.to_csv(r'tables/classstd.csv')
+classcor.to_csv(r'tables/classcorr.csv')
+
 # Print the details to the screen. 
-	print("\n1. Dataset COUNT. This table shows the number of records per characteristic grouped by species in the dataset.\n",classcount)
-	print("\n2. Dataset MEAN. This table shows the mean of the characteristics grouped by species in the dataset.\n",classmean)
-	print("\n3. Dataset MAX. This table shows the max of the characteristics grouped by species in the dataset.\n",classmax)
-	print("\n4. Dataset MIN. This table shows the min of the characteristics grouped by species in the dataset.\n",classmin)
-	print("\n3. Dataset MED. This table shows the median of the characteristics grouped by species in the dataset.\n",classmedian)
-	print("\n3. Dataset STD.DEV. This table shows the standard deviation of the characteristics grouped by species in the dataset.\n",classstd)
-	return
-
-if __name__ == "__main__":
-		main()
-
-#sns.pairplot(irisdf, hue='Class', height=2.0).fig.suptitle("Iris Data Scatter Plots", fontsize=20)
-#irisdf.plot()
-#plt.show(block=True)
-
-#print(irisdf.groupby(pd.Grouper(key="Class")).mean())
+print('\n1. Dataset COUNT. This table shows the number of records per characteristic grouped by species in the dataset.\n',classcount)
+print('\n2. Dataset MEAN. This table shows the mean of the characteristics grouped by species in the dataset.\n',classmean)
+print('\n3. Dataset MAX. This table shows the max of the characteristics grouped by species in the dataset.\n',classmax)
+print('\n4. Dataset MIN. This table shows the min of the characteristics grouped by species in the dataset.\n',classmin)
+print('\n3. Dataset MED. This table shows the median of the characteristics grouped by species in the dataset.\n',classmedian)
+print('\n3. Dataset Coorelation. This table shows the pairwuse correlation of the characteristics grouped by species in the dataset.\n',classcor)
 
 
-
-#irisdf.plot()
-#irisdf.plot.hist(stacked=True)
-#irisdf.diff().hist(color="k", alpha=0.5, bins=50)
-#df1 = irisdf.boxplot(by="Class", grid=False, rot=45, fontsize=15)
-#irisdf.plot.area()
-#ax = irisdf.plot.scatter(x="Petal_Length", y = "Class", color="DarkBlue", label = "PL")
-#irisdf.plot.scatter(x="Sepal_Width", y = "Class", color="DarkGreen", label = "PW", ax = ax)
-#from pandas.plotting import scatter_matrix
-#scatter_matrix(irisdf, alpha=0.2, figsize=(6, 6), diagonal="kde")
-
-#plt.show()
+# All of the code below generate and outputs the plots from the data.
+# This code generates a full pair plot comparing all of the characteristics against each other across the 3 species. 
+#https://seaborn.pydata.org/tutorial/axis_grids.html
+sns.pairplot(irisdf, hue='Class', height=2.0)
+plt.savefig(r'plots/irispairplot.png')
 
 
-''' plt.figure(facecolor = 'lightcyan') 
+plt.clf()
 
-# This section creates and formats the individual plot lines. # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.plot.html.
-# It is a tiny subset of the vast number of options availabel for plotting. 
-# First is the line style and color, MS is line marker size, mec and mfc set the markers face and edge colors, label applies a legend to the plot . 
-plt.plot(x, x, 'o-r', ms=10, mec = 'b', mfc = 'r', label = 'Linear f(x)')
-plt.plot(x, x**2, 'o--g', ms=10, mec = 'r', mfc = 'b', label = 'Quadratic g(x^2)')
-plt.plot(x, x**3, 'o-.b', ms=10, mec = 'b', mfc = 'g', label = 'Cubed (h^2)')
+# Overall Petal Length vs Petal Width
+sns.histplot(irisdf["Petal_Length"], color="green", label="Petal_Length")
+sns.histplot(irisdf["Petal_Width"], color="blue", label= "Petal_width")
+plt.xlabel("")
+plt.legend()
+plt.savefig(r'plots/histcompare_petalL_petalW.png')
 
-#This section formats the plot axes, titles, gridlines, fontsize and displays the resulting plot using various options to test out different formatting. 
-ax = plt.axes()
-ax.set_facecolor('bisque')
-plt.xticks(x)
-plt.legend(fontsize = 15)
-plt.title('Week 8 Lab Plotting', fontsize = 20, fontweight = 'bold', style = 'italic')
-plt.xlabel('Dimension (x)', fontsize = 15)
-plt.ylabel('Function(y)', fontsize = 15)
-plt.grid(linestyle = 'dashed')
-plt.show() '''
 
+plt.cla()
+
+#Overall Sepal Length vs Sepal Width
+sns.histplot(irisdf["Sepal_Length"], color="green", label="Sepal_Length")
+sns.histplot(irisdf["Sepal_Width"], color="blue", label= "Sepal_width")
+plt.xlabel("")
+plt.legend()
+plt.savefig(r'plots/histcompare_sepalL_sepalW.png')
+
+
+plt.clf()
+
+
+# Comparing Sepal Length across the species. 
+g=sns.FacetGrid(irisdf, col="Class", hue="Class")
+g.map(sns.histplot, "Sepal_Length")
+plt.savefig(r'plots/histsepalLen_species.png')
+
+plt.clf()
+# Comparing Sepal Width across the species. 
+g=sns.FacetGrid(irisdf, col="Class", hue="Class")
+g.map(sns.histplot, "Sepal_Width")
+plt.savefig(r'plots/histsepalWid_species.png')
+
+plt.clf()
+# Comparing Petal Length across the species. 
+g=sns.FacetGrid(irisdf, col="Class", hue="Class")
+g.map(sns.histplot, "Petal_Length")
+plt.savefig(r'plots/histpetalLen_species.png')
+
+plt.clf()
+# Comparing Petal Width across the species. 
+g=sns.FacetGrid(irisdf, col="Class", hue="Class")
+g.map(sns.histplot, "Petal_Width")
+plt.savefig(r'plots/histpetalWid_species.png')
+
+plt.clf()
+
+#Complete set of Boxplats across the species. https://dev.to/thalesbruno/subplotting-with-matplotlib-and-seaborn-5ei8
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+fig.suptitle("Boxplots of Characteristics across the Species")
+sns.boxplot(ax=axes[0, 0], data=irisdf, x='Class', y='Sepal_Length')
+sns.boxplot(ax=axes[0, 1], data=irisdf, x='Class', y='Sepal_Width')
+sns.boxplot(ax=axes[1, 0], data=irisdf, x='Class', y='Petal_Length')
+sns.boxplot(ax=axes[1, 1], data=irisdf, x='Class', y='Petal_Width')
+plt.savefig(r'plots/boxplots_species.png')
+
+#Regression Testing Plots: https://seaborn.pydata.org/generated/seaborn.lmplot.html
+# Regression Petal L vs W
+sns.lmplot(x="Petal_Length", y="Petal_Width", col="Class", hue="Class", data=irisdf)
+plt.savefig(r'plots/regression_Petal_length_width.png')
+
+# Regression Sepal L vs W
+sns.lmplot(x="Sepal_Length", y="Sepal_Width", col="Class", hue="Class", data=irisdf)
+plt.savefig(r'plots/regression_Sepal_length_width.png')
+
+# Regression Petal L vs Sepal L
+sns.lmplot(x="Petal_Length", y="Sepal_Length", col="Class", hue="Class", data=irisdf)
+plt.savefig(r'plots/regression_length_Petal_Sepal.png')
+
+# Regression Petal W vs Sepal W
+sns.lmplot(x="Petal_Width", y="Sepal_Width", col="Class", hue="Class", data=irisdf)
+plt.savefig(r'plots/regression_width_Petal_Sepal.png')
